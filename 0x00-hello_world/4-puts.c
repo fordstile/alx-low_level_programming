faf1b16c8e9e2ea714afdadfee5681376f25abdf
fordstile <masterland9772@gmail.com>
2023-11-11 00:46:33 -0800

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	char str[] =("\"Programming is like building a multilingual puzzle");
+
+	puts(char);
 	return (0);
 }
