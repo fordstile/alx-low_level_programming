ca33ac64e27f2a715525a59262e59d031251b7f7
fordstile <masterland9772@gmail.com>
2024-01-11 11:19:00 -0800

    alx task

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,26 +1,30 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - Retrieves a node from a doubly linked list at a specific index.
+ * get_dnodeint_at_index - Retrieves a node from a doubly linked list
+ * at a specific index.
  * @head: Pointer to the first node of the linked list.
  * @index: The index of the node to be retrieved.
- * Return: The address of the node at the specified index, or NULL if it doesn't exist.
+ * Return: The address of the node at the specified index, or NULL
+ * if it doesn't exist.
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
+	/*
+	 * Traverse the list until the specified index is reached or
+	 * the end of the list.
+	 */
+	while (head != NULL && i < index)
+	{
+		head = head->next;
+		i++;
+	}
 
-    /* Traverse the list until the specified index is reached or the end of the list */
-    while (head != NULL && i < index)
-    {
-        head = head->next;
-        i++;
-    }
+	/* If the index matches, return the current node */
+	if (i == index)
+		return (head);
 
-    /* If the index matches, return the current node */
-    if (i == index)
-        return head;
-
-    /* If the node at the specified index is not found, return NULL */
-    return NULL;
+	/* If the node at the specified index is not found, return NULL */
+	return (NULL);
 }
