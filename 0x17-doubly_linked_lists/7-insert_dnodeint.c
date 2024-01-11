ac95459abefac074409ad37655c94d0bf146fc7b
fordstile <masterland9772@gmail.com>
2024-01-11 11:28:54 -0800

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,42 +11,42 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *current;
-    unsigned int i;
+	dlistint_t *new_node, *current;
+	unsigned int i;
 
-    if (h == NULL)
-        return NULL;
+	if (h == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return NULL;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    if (idx == 0)
-    {
-        new_node->next = *h;
-        new_node->prev = NULL;
-        if (*h != NULL)
-            (*h)->prev = new_node;
-        *h = new_node;
-        return new_node;
-    }
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 
-    current = *h;
-    for (i = 0; current != NULL && i < idx - 1; i++)
-        current = current->next;
+	current = *h;
+	for (i = 0; current != NULL && i < idx - 1; i++)
+		current = current->next;
 
-    if (current == NULL || (current->next == NULL && i < idx - 1))
-    {
-        free(new_node);
-        return NULL;
-    }
+	if (current == NULL || (current->next == NULL && i < idx - 1))
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->next = current->next;
-    new_node->prev = current;
-    if (current->next != NULL)
-        current->next->prev = new_node;
-    current->next = new_node;
+	new_node->next = current->next;
+	new_node->prev = current;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
 
-    return new_node;
+	return (new_node);
 }
