02c68ca201f43d5a46c6cdbb60e88babce95ddf7
fordstile <masterland9772@gmail.com>
2024-01-11 11:05:19 -0800

    alx task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,31 +10,31 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    dlistint_t *current;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *current;
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
-    if (*head == NULL) /* If the list is empty, insert at the beginning */
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-        return (new_node);
-    }
+	if (*head == NULL) /* If the list is empty, insert at the beginning */
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
 
-    /* Navigate to the end of the list */
-    current = *head;
-    while (current->next != NULL)
-    {
-        current = current->next;
-    }
+	/* Navigate to the end of the list */
+	current = *head;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
 
-    current->next = new_node;
-    new_node->prev = current;
+	current->next = new_node;
+	new_node->prev = current;
 
-    return (new_node);
+	return (new_node);
 }
