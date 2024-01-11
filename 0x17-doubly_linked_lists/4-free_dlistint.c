c7ababa98357ad9d3e0d9567f4d904ba92782139
fordstile <masterland9772@gmail.com>
2024-01-11 11:10:46 -0800

    task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -9,12 +9,12 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *next_node;
+	dlistint_t *next_node;
 
-    while (head != NULL)
-    {
-        next_node = head->next;
-        free(head);
-        head = next_node;
-    }
+	while (head != NULL)
+	{
+		next_node = head->next;
+		free(head);
+		head = next_node;
+	}
 }
