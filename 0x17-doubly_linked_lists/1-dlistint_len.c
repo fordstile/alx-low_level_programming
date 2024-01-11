6d6c780200c66b700666884f5e1f7fefa8698729
fordstile <masterland9772@gmail.com>
2024-01-11 09:09:39 -0800

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Returns the number of elements in a linked list
+ * @h: Pointer to the first node of the linked list
+ *
+ * Return: The number of elements in the linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t count = 0;
+
+    for (; h; h = h->next)
+    {
+        count++;
+    }
+
+    return count;
+}
